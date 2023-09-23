a294ddd552cfb18ead5bc4279d43dd392a2e7269
michaelparkernorton <michaelparkernorton.va@gmail.com>
2023-09-23 15:46:01 -0400

    updated branch

--- src/js/product.js
+++ src/js/product.js
@@ -1,8 +1,21 @@
 import { setLocalStorage } from "./utils.mjs";
+import { getLocalStorage } from "./utils.mjs";
 import { findProductById } from "./productData.mjs";
 
+
+// products are being allow to be additionally added
+// this is unnessary as there is a quanity feature
+let cart = getLocalStorage("so-cart");
+let product_added = false;
+
 function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
+  if (!product_added) {
+    cart.push(product);
+    // console.log(cart);
+    setLocalStorage("so-cart", cart);
+    console.log(getLocalStorage("so-cart"));
+    product_added = true;
+  }
 }
 // add to cart button event handler
 async function addToCartHandler(e) {
