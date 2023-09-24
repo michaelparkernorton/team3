711c8d88ecf19cacbfda24b61a205bb45339a2a6
wesvane1 <113637824+wesvane1@users.noreply.github.com>
2023-09-23 18:53:18 -0600

    Fixed the cart, I think

--- src/js/product.js
+++ src/js/product.js
@@ -1,8 +1,18 @@
 import { setLocalStorage } from "./utils.mjs";
+import { getLocalStorage } from "./utils.mjs";
 import { findProductById } from "./productData.mjs";
 
+let cart = getLocalStorage("so-cart")
+// The below line logs out all data points affiliated with this specific item
+// console.log(cart)
+let cartItems = [];
+
 function addProductToCart(product) {
+  cartItems.push(product);
+  // The below line logs out all data points affiliated with this specific item
+  // console.log(cartItems);
   setLocalStorage("so-cart", product);
+  console.log(getLocalStorage("so-cart"));
 }
 // add to cart button event handler
 async function addToCartHandler(e) {
