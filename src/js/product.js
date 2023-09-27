d66b473fe9a55c0aaf79d93afd4e27f9b83ee7dd
wesvane1 <113637824+wesvane1@users.noreply.github.com>
2023-09-27 14:50:46 -0600

    fixed another error

--- src/js/product.js
+++ src/js/product.js
@@ -1,6 +1,6 @@
 import { setLocalStorage, getLocalStorage, getParam } from './utils.mjs';
 import { findProductById } from './productData.mjs';
-import { productDetails, renderProductDetails } from './productDetails.mjs';
+import { productDetails } from './productDetails.mjs';
 
 const productId = getParam('product');
 productDetails(productId);
