24cc10feacb3a43511cc6876f85e9ec828af16d4
wesvane1 <113637824+wesvane1@users.noreply.github.com>
2023-09-27 13:49:41 -0600

    added details to productDetails.mjs

---
+++ src/js/productDetails.mjs
@@ -0,0 +1,23 @@
+import { setLocalStorage, getLocalStorage, getParam } from './utils.mjs';
+
+
+function productDetails(productID){
+
+}
+
+function renderProductDetails(){
+
+}
+
+function addProductToCart(product) {
+  // get existing cart data from local storage
+  let cart = getLocalStorage('so-cart');
+  //if cart is empty, initialize as empty array
+  if(!Array.isArray(cart)){
+    cart = [];
+  }
+  //Add new product to cart
+  cart.push(product);
+  //Update cart in local storage
+  setLocalStorage('so-cart', cart);
+}
