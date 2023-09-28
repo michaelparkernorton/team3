6ddcce7cd02f3dc04bfad04643bc6d3b92585fe5
wesvane1 <113637824+wesvane1@users.noreply.github.com>
2023-09-28 12:53:59 -0600

    Finished team activity 2

--- src/js/product.js
+++ src/js/product.js
@@ -1,31 +1,5 @@
-import { setLocalStorage, getLocalStorage, getParam } from './utils.mjs';
-import { findProductById } from './productData.mjs';
-// import { productDetails } from './productDetails.mjs';
+import { getParam } from './utils.mjs';
+import  productDetails  from './productDetails.mjs';
 
 const productId = getParam('product');
 productDetails(productId);
-// renderProductDetails();
-
-
-function addProductToCart(product) {
-  // get existing cart data from local storage
-  let cart = getLocalStorage('so-cart');
-  //if cart is empty, initialize as empty array
-  if(!Array.isArray(cart)){
-    cart = [];
-  }
-  //Add new product to cart
-  cart.push(product);
-  //Update cart in local storage
-  setLocalStorage('so-cart', cart);
-}
-// add to cart button event handler
-async function addToCartHandler(e) {
-  const product = await findProductById(e.target.dataset.id);
-  addProductToCart(product);
-}
-
-// add listener to Add to Cart button
-document
-  .getElementById('addToCart')
-  .addEventListener('click', addToCartHandler);
--- src/js/productDetails.mjs
+++ src/js/productDetails.mjs
@@ -1,33 +1,30 @@
-import { setLocalStorage, getLocalStorage, getParam } from './utils.mjs'; 
+import { setLocalStorage} from './utils.mjs';
+import { findProductById } from './productData.mjs';
 
+let product = {};
 
-export default async function productDetails(productId, selector) {
+export default async function productDetails(productId) {
   // use findProductById to get the details for the current product. findProductById will return a promise! use await or .then() to process it
+  product = await findProductById(productId);
   // once we have the product details we can render out the HTML
+  renderProductDetails();
   // add a listener to Add to Cart button
-  const findItem = await findProductById(productId);
-  return findItem.find
+  document.getElementById("addToCart").addEventListener("click", addToCart);
 }
 
-// export async function findProductById(id) {
-//   const products = await getData();
-//   return products.find((item) => item.Id === id);
-// }
+export function renderProductDetails(){
+  document.querySelector('#productName').innerText = product.Brand.Name;
+  document.querySelector('#productNameWithoutBrand').innerText = product.NameWithoutBrand;
+  document.querySelector('#productImage').src = product.Image;
+  document.querySelector('#productImage').alt = product.Name;
+  document.querySelector('#productFinalPrice').innerText = product.FinalPrice;
+  document.querySelector('#productColorName').innerText = product.Colors[0].ColorName;
+  document.querySelector('#productDescriptionHtmlSimple').innerHTML = product.DescriptionHtmlSimple;
+  console.log(product);
+}
 
-// export function renderProductDetails(){
-//   const productName = document.querySelector('productName');
-//   productName.innerHTML = "Ajax";
-// }
-
-function addProductToCart(product) {
-  // get existing cart data from local storage
-  let cart = getLocalStorage('so-cart');
-  //if cart is empty, initialize as empty array
-  if(!Array.isArray(cart)){
-    cart = [];
-  }
-  //Add new product to cart
-  cart.push(product);
-  //Update cart in local storage
-  setLocalStorage('so-cart', cart);
+function addToCart()
+{
+  setLocalStorage('so-cart', product);
+  
 }
