f4fa6e3b2ef882c456c0d79b6d3023ff0a65c65c
wesvane1 <113637824+wesvane1@users.noreply.github.com>
2023-09-29 16:00:07 -0600

    added the cart subtotal.

--- src/js/cart.js
+++ src/js/cart.js
@@ -3,12 +3,23 @@ import { getLocalStorage } from './utils.mjs';
 
 function renderCartContents() {
   const cartItems = getLocalStorage('so-cart') || [];
+  // Set an item = to 0 so you can add to it
+  let subTotal = 0;
+  // for loop just adds all Final Prices together in cartItems
+  for (let i = 0; i < cartItems.length; i++) {
+    const price = cartItems[i].FinalPrice;
+    subTotal+=price
+  }
   //check if cartItems is an array before mapping
   if (cartItems.length > 0) {
     const htmlItems = cartItems.map((item) => cartItemTemplate(item));
     document.querySelector('.product-list').innerHTML = htmlItems.join('');
+    // Displays cart total
+    // TODO I want this element centered, or at least shifted to the left a little. It may look better that way.
+    document.querySelector('.cart-total').innerHTML = 'Cart Total: $' + subTotal;
   } else {
   document.querySelector('.product-list').innerHTML = 'Your cart is empty.';
+  document.querySelector('.cart-total').innerHTML = '';
   }
 }
 
