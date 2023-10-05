9be83840774a83fbb2153cff6c23120c43a1ce57
doctorzik <ezekieljeremiah92@gmail.com>
2023-10-06 00:22:01 +0100

    Error handler

--- src/js/productDetails.mjs
+++ src/js/productDetails.mjs
@@ -1,5 +1,5 @@
-import { getLocalStorage, setLocalStorage} from './utils.mjs';
-import { findProductById } from './productData.mjs';
+import { getLocalStorage, setLocalStorage } from "./utils.mjs";
+import { findProductById } from "./productData.mjs";
 
 let product = {};
 
@@ -7,27 +7,45 @@ export default async function productDetails(productId) {
   // use findProductById to get the details for the current product. findProductById will return a promise! use await or .then() to process it
   product = await findProductById(productId);
   // once we have the product details we can render out the HTML
-  renderProductDetails();
+  try {
+    renderProductDetails(); // This function does not account for invalid products name.
+  } catch (err) {
+    // the error is been handled wih this try catch block
+    if (err) {
+      document.querySelector(".product-detail").innerHTML =
+        "The product is not currently available"; // this message is displaped to the user if error is found
+    }
+  }
+
   // add a listener to Add to Cart button
-  document.getElementById('addToCart').addEventListener('click', addToCart);
+  document.getElementById("addToCart").addEventListener("click", addToCart);
 }
 
 export function renderProductDetails() {
-  document.querySelector('#productName').innerText = product.Brand.Name;
-  document.querySelector('#productNameWithoutBrand').innerText = product.NameWithoutBrand;
-  document.querySelector('#productImage').src = product.Image;
-  document.querySelector('#productImage').alt = product.Name;
-  let percentageOff = (product.SuggestedRetailPrice-product.FinalPrice)/product.SuggestedRetailPrice * 100;
-  document.querySelector('#productDiscount').innerText = "-" + percentageOff.toFixed(0) + "%";
-  document.querySelector('#productFinalPrice').innerText = "$" + product.FinalPrice;
-  document.querySelector('#productSuggestedRetailPrice').innerText = "$" + product.SuggestedRetailPrice.toFixed(2);
-  document.querySelector('#productColorName').innerText = product.Colors[0].ColorName;
-  document.querySelector('#productDescriptionHtmlSimple').innerHTML = product.DescriptionHtmlSimple;
+  document.querySelector("#productName").innerText = product.Brand.Name;
+  document.querySelector("#productNameWithoutBrand").innerText =
+    product.NameWithoutBrand;
+  document.querySelector("#productImage").src = product.Image;
+  document.querySelector("#productImage").alt = product.Name;
+  let percentageOff =
+    ((product.SuggestedRetailPrice - product.FinalPrice) /
+      product.SuggestedRetailPrice) *
+    100;
+  document.querySelector("#productDiscount").innerText =
+    "-" + percentageOff.toFixed(0) + "%";
+  document.querySelector("#productFinalPrice").innerText =
+    "$" + product.FinalPrice;
+  document.querySelector("#productSuggestedRetailPrice").innerText =
+    "$" + product.SuggestedRetailPrice.toFixed(2);
+  document.querySelector("#productColorName").innerText =
+    product.Colors[0].ColorName;
+  document.querySelector("#productDescriptionHtmlSimple").innerHTML =
+    product.DescriptionHtmlSimple;
 }
 
 function addToCart() {
-  const cartItems = getLocalStorage('so-cart') || [];
-  cartItems.push(product);// push new item into the array
-    // TODO: update existing cart item
-  setLocalStorage('so-cart', cartItems);
+  const cartItems = getLocalStorage("so-cart") || [];
+  cartItems.push(product); // push new item into the array
+  // TODO: update existing cart item
+  setLocalStorage("so-cart", cartItems);
 }
