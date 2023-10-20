9fc23e577644e5b2a779dec1f5f82c1b5e9d2607
Mike <michaelparkernorton.va@gmail.com>
2023-10-19 21:38:53 -0400

    fixed?

--- src/js/productList.mjs
+++ src/js/productList.mjs
@@ -1,10 +1,6 @@
 import { getData } from "/js/productData.mjs";
 // import { renderListWithTemplate } from "./utils.mjs";
 
-
-
-
-
 // function productCardTemplate(product) {
 //   if (
 //     product.Id === "880RR" ||
@@ -35,11 +31,11 @@ import { getData } from "/js/productData.mjs";
 //   }
 // }
 
-// export default async function productList(selector, category) {
-//   // get the element we will insert the list into from the selector
-//   const el = document.querySelector(selector);
-//   // get the list of products
-//   const list = await getData(category);
-//   // render out the product list to the element
-//   renderListWithTemplate(productCardTemplate, el, list, "afterbegin");
-// }
+export default async function productList(selector, category) {
+  // get the element we will insert the list into from the selector
+  const el = document.querySelector(selector);
+  // get the list of products
+  const list = await getData(category);
+  // render out the product list to the element
+  //   renderListWithTemplate(productCardTemplate, el, list, "afterbegin");
+}
