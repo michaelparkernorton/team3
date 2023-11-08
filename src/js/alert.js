a10d656ec7046819cecf35107f82ee8ede87c410
doctorzik <ezekieljeremiah92@gmail.com>
2023-11-08 23:00:05 +0100

    Finished the customizable alert

---
+++ src/js/alert.js
@@ -0,0 +1,19 @@
+export function Func() {
+  let urlOfJsonFile = "../public/json/alerts.json";
+  fetch(urlOfJsonFile)
+    .then((res) => {
+      return res.json();
+    })
+    .then((data) => {
+      const alertsContainer = document.querySelector("main");
+
+      data.forEach((item) => {
+        const alertDiv = document.createElement("div");
+        alertDiv.classList.add("alert");
+        alertDiv.textContent = item.message;
+        alertDiv.style.backgroundColor = item.color;
+
+        alertsContainer.prepend(alertDiv);
+      });
+    });
+}
