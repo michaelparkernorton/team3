a715817234af79ffbda820b58b214a38a90d59a6
Mike <michaelparkernorton.va@gmail.com>
2023-11-09 08:26:32 -0500

    imported cartChange

--- src/js/mjs/cartChange.mjs
+++ src/js/mjs/cartChange.mjs
@@ -1,21 +1,20 @@
+export function cartChange() {
+  document.addEventListener("DOMContentLoaded", function () {
+    const cart = document.querySelector(".cart");
 
-document.addEventListener('DOMContentLoaded', function() {
-    const cart = document.querySelector('.cart'); 
-  
     function cartChange() {
       if (cart) {
-        cart.classList.add('added');
-    
+        cart.classList.add("added");
+
         setTimeout(() => {
-          cart.classList.remove('added');
+          cart.classList.remove("added");
         }, 5000);
       }
     }
-  
-   
-    const addToCartButton = document.getElementById('addToCart');
+
+    const addToCartButton = document.getElementById("addToCart");
     if (addToCartButton) {
-      addToCartButton.addEventListener('click', cartChange);
+      addToCartButton.addEventListener("click", cartChange);
     }
   });
-  
+}
--- src/js/product.js
+++ src/js/product.js
@@ -1,7 +1,9 @@
 import { getParam } from "./mjs/utils.mjs";
 import productDetails from "./mjs/productDetails.mjs";
 import { loadHeaderFooter } from "./mjs/utils.mjs";
+import { cartChange } from "./mjs/cartChange.mjs";
 
 loadHeaderFooter();
 const productId = getParam("product");
 productDetails(productId);
+cartChange();
